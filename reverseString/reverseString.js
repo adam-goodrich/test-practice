1ce4f887103c6a53b015e4acdc3427ebff43e2ad
Adam Goodrich <agoodrich57@gmail.com>
2021-07-12 13:09:36 -0400

    added gitnore

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,4 +1,7 @@
 const reverseString = (string) => {
+  if (typeof string != "string") {
+    return "You must provide a string";
+  }
   return string.split("").reverse().join("");
 };
 module.exports = reverseString;
--- reverseString/reverseString.test.js
+++ reverseString/reverseString.test.js
@@ -4,6 +4,30 @@ test("Function is defined", () => {
   expect(reverseString("hello")).toBeDefined();
 });
 
+test("Alerts if no args", () => {
+  expect(reverseString()).toBe("You must provide a string");
+});
+
+test("Alerts if boolean", () => {
+  expect(reverseString(true)).toBe("You must provide a string");
+});
+
+test("Alerts if null", () => {
+  expect(reverseString(null)).toBe("You must provide a string");
+});
+
+test("Alerts if undefined", () => {
+  expect(reverseString(undefined)).toBe("You must provide a string");
+});
+
+test("Alerts if nan", () => {
+  expect(reverseString(NaN)).toBe("You must provide a string");
+});
+
+test("Numbers should not work", () => {
+  expect(reverseString(3)).toBe("You must provide a string");
+});
+
 test("Function is defined", () => {
   expect(reverseString("hello")).toBe("olleh");
 });
