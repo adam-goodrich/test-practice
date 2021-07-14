39453b648182148a8d22f04bdfbe0b097c4824ca
Adam Goodrich <agoodrich57@gmail.com>
2021-07-14 10:26:59 -0400

    added logic and tests

--- ArrayAnalysis/arrayAnalysis.js
+++ ArrayAnalysis/arrayAnalysis.js
@@ -1,5 +1,13 @@
 const arrayAnalysis = (array) => {
-  return array;
+  const average = array.reduce((a, b) => a + b, 0) / array.length;
+  const sorted = array.sort((a, b) => a - b);
+  const object = {
+    average: average,
+    min: sorted[0],
+    max: sorted[sorted.length - 1],
+    length: sorted.length,
+  };
+  return object;
 };
 
 module.exports = arrayAnalysis;
--- ArrayAnalysis/arrayAnalysis.test.js
+++ ArrayAnalysis/arrayAnalysis.test.js
@@ -3,3 +3,12 @@ const arrayAnalysis = require("./arrayAnalysis");
 test("function is defined", () => {
   expect(arrayAnalysis([])).toBeDefined();
 });
+
+test("function is defined", () => {
+  expect(arrayAnalysis([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
