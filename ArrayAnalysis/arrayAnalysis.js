35a7866a94780bc1c45ae7a4a64805306acd5f68
Adam Goodrich <agoodrich57@gmail.com>
2021-07-14 11:07:33 -0400

    added more test

--- ArrayAnalysis/arrayAnalysis.js
+++ ArrayAnalysis/arrayAnalysis.js
@@ -1,13 +1,23 @@
 const arrayAnalysis = (array) => {
+  if (!Array.isArray(array)) {
+    return "must provide an array";
+  }
+  if (array.length == 0) {
+    return {
+      average: 0,
+      min: 0,
+      max: 0,
+      length: 0,
+    };
+  }
   const average = array.reduce((a, b) => a + b, 0) / array.length;
   const sorted = array.sort((a, b) => a - b);
-  const object = {
+  return {
     average: average,
     min: sorted[0],
     max: sorted[sorted.length - 1],
     length: sorted.length,
   };
-  return object;
 };
 
 module.exports = arrayAnalysis;
--- ArrayAnalysis/arrayAnalysis.test.js
+++ ArrayAnalysis/arrayAnalysis.test.js
@@ -4,7 +4,56 @@ test("function is defined", () => {
   expect(arrayAnalysis([])).toBeDefined();
 });
 
-test("function is defined", () => {
+test("function only accepts arrays", () => {
+  expect(arrayAnalysis(false)).toBeDefined();
+});
+
+test("function returns correct answers length 1", () => {
+  expect(arrayAnalysis([1])).toEqual({
+    average: 1,
+    min: 1,
+    max: 1,
+    length: 1,
+  });
+});
+
+test("function returns correct empty array", () => {
+  expect(arrayAnalysis([])).toEqual({
+    average: 0,
+    min: 0,
+    max: 0,
+    length: 0,
+  });
+});
+
+test("function returns correct all 0's", () => {
+  expect(arrayAnalysis([0, 0, 0, 0, 0])).toEqual({
+    average: 0,
+    min: 0,
+    max: 0,
+    length: 5,
+  });
+});
+
+test("function returns correct 1 zero", () => {
+  expect(arrayAnalysis([0])).toEqual({
+    average: 0,
+    min: 0,
+    max: 0,
+    length: 1,
+  });
+});
+
+test("function returns correct answers", () => {
+  expect(arrayAnalysis([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
+});
+
+test("function returns correct answers", () => {
   expect(arrayAnalysis([1, 8, 3, 4, 2, 6])).toEqual({
     average: 4,
     min: 1,
