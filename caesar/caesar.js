f390d322f43b2dc343d20b80c8b1b5f80035b4dc
Adam Goodrich <agoodrich57@gmail.com>
2021-07-14 09:18:46 -0400

    added caesar solution andmore tests

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,26 @@
-const caesar = (string) => {
-  return true;
+const caesar = (string, num) => {
+  let result = [];
+  let stringArray = string.split("");
+  let ascii_mapped_list = [];
+  let newAsciiNum;
+  if (num < 0) {
+    num = (num % 26) + 26;
+  }
+  stringArray.forEach((char) => ascii_mapped_list.push(char.charCodeAt()));
+  ascii_mapped_list.forEach((char, index) => {
+    if (char >= 65 && char <= 90) {
+      char = char - 65;
+      newAsciiNum = (char + num) % 26;
+      result.push(String.fromCharCode(newAsciiNum + 65));
+    } else if (char >= 97 && char <= 122) {
+      char = char - 97;
+      newAsciiNum = (char + num) % 26;
+      result.push(String.fromCharCode(newAsciiNum + 97));
+    } else {
+      result.push(String.fromCharCode(char));
+    }
+  });
+  return result.join("");
 };
 
 module.exports = caesar;
--- caesar/caesar.test.js
+++ caesar/caesar.test.js
@@ -3,3 +3,11 @@ const caesar = require("./caesar");
 test("function is defined", () => {
   expect(caesar("hello")).toBeDefined();
 });
+
+test("function is defined", () => {
+  expect(caesar("")).toBe("");
+});
+
+test("function is defined", () => {
+  expect(caesar("Hello World!", 1)).toBe("Ifmmp Xpsme!");
+});
