8837c7b1d8414ad57fa4e6fddde007e156a68a41
Adam Goodrich <agoodrich57@gmail.com>
2021-07-14 09:29:48 -0400

    added more tests

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,23 +1,22 @@
 const caesar = (string, num) => {
   let result = [];
   let stringArray = string.split("");
-  let ascii_mapped_list = [];
   let newAsciiNum;
   if (num < 0) {
     num = (num % 26) + 26;
   }
-  stringArray.forEach((char) => ascii_mapped_list.push(char.charCodeAt()));
-  ascii_mapped_list.forEach((char, index) => {
-    if (char >= 65 && char <= 90) {
-      char = char - 65;
-      newAsciiNum = (char + num) % 26;
+  let asciiMappedList = stringArray.map((char) => char.charCodeAt());
+  asciiMappedList.forEach((charNum) => {
+    if (charNum >= 65 && charNum <= 90) {
+      charNum = charNum - 65;
+      newAsciiNum = (charNum + num) % 26;
       result.push(String.fromCharCode(newAsciiNum + 65));
-    } else if (char >= 97 && char <= 122) {
-      char = char - 97;
-      newAsciiNum = (char + num) % 26;
+    } else if (charNum >= 97 && charNum <= 122) {
+      charNum = charNum - 97;
+      newAsciiNum = (charNum + num) % 26;
       result.push(String.fromCharCode(newAsciiNum + 97));
     } else {
-      result.push(String.fromCharCode(char));
+      result.push(String.fromCharCode(charNum));
     }
   });
   return result.join("");
--- caesar/caesar.test.js
+++ caesar/caesar.test.js
@@ -4,10 +4,26 @@ test("function is defined", () => {
   expect(caesar("hello")).toBeDefined();
 });
 
-test("function is defined", () => {
+test("works with blank string", () => {
   expect(caesar("")).toBe("");
 });
 
-test("function is defined", () => {
+test("Works with low number", () => {
+  expect(caesar("Hello", 1)).toBe("Ifmmp");
+});
+
+test("Works with spaces", () => {
+  expect(caesar("Hello World", 1)).toBe("Ifmmp Xpsme");
+});
+
+test("Works with puncuation", () => {
   expect(caesar("Hello World!", 1)).toBe("Ifmmp Xpsme!");
+});
+
+test("Works with negative numbers", () => {
+  expect(caesar("Hello", -1)).toBe("Gdkkn");
+});
+
+test("Works with large numbers", () => {
+  expect(caesar("Hello", 500)).toBe("Nkrru");
 });
